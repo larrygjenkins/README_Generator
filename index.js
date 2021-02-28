d12e5dfa6a4955405dbaa0c9da387b5bac7ba76b
Larry Jenkins <75516987+larrygjenkins@users.noreply.github.com>
2021-02-27 19:21:51 -0800

    added list question for license type

--- index.js
+++ index.js
@@ -21,37 +21,47 @@ inquirer
             type: "input",
             name: "install",
             message: "What steps are required to install your project?",
+        },
+
+        {
+            type: "input",
+            name: "usage",
+            message: "How should your project be used?",
+        },
+
+        {
+            type: "input",
+            name: "contributors",
+            message: "List all contributors for this project.",
+        },
+
+        {
+            type: "list",
+            name: "license",
+            message: "What type of license should apply to your project?",
+            choices: ["MIT", "Apache", "GPL"],
+            filter: function (val) {
+                return val.toLowerCase();
+            }
+        },
+
+        {
+            type: "input",
+            name: "testing",
+            message: "List any specific testing instructions for your project. If there are none, enter 'There are no specific testing requirements.'",
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
         }
-
-        // {
-        //     type: "input",
-        //     name: "usage",
-        //     message: "What is the title of your project?",
-        // },
-
-        // {
-        //     type: "input",
-        //     name: "title",
-        //     message: "What is the title of your project?",
-        // },
-
-        // {
-        //     type: "input",
-        //     name: "title",
-        //     message: "What is the title of your project?",
-        // },
-
-        // {
-        //     type: "input",
-        //     name: "title",
-        //     message: "What is the title of your project?",
-        // },
-
-        // {
-        //     type: "input",
-        //     name: "title",
-        //     message: "What is the title of your project?",
-        // }
     ])
     .then((answers) => {
         console.log(answers);
